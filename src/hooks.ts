e6abfe41af35137a917dc6603ab021acbfe7413a
Tobias <tobias@sernhede.com>
2024-01-12 13:24:58 +0100

    Initial commit

---
+++ src/hooks.ts
@@ -0,0 +1,7 @@
+import type { Reroute } from '@sveltejs/kit';
+
+export const reroute: Reroute = ({ url }) => {
+	// if (url.pathname in translated) {
+	// 	return translated[url.pathname];
+	// }
+};
